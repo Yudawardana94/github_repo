7638839fa3f63f46af45fbd7155b1de131a151b0
yudawardana <yuda.wardana40@gmail.com>
2020-09-10 20:26:01 +0700

    [ADD] show data on search

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import {connect} from 'react-redux'
-import {InputGroup, FormControl, Button, Container} from 'react-bootstrap'
+import {InputGroup, FormControl, Button, Container, Card, CardGroup} from 'react-bootstrap'
 
 import {getUserRepos} from '../../stores/actions'
 
@@ -22,7 +22,7 @@ function Home(props) {
 
     return (
         <Container>
-            <h1> home </h1>
+            <h2> Search git Repos</h2>
             <InputGroup className="mb-3">
                 <FormControl
                 placeholder="type user`s name here..."
@@ -45,9 +45,29 @@ function Home(props) {
             </h3>
             {
                 props?.userRepo.map((el, idx) => {
-                    return <div key={idx}>
-                        <p>{el.id}</p>
-                    </div>
+                    return <Card style={{width: '18rem', margin: 15 }}>
+                            <Card.Img variant="top" src={el.owner.avatar_url} style={{width: '100%'}} />
+                            <Card.Body>
+                                <Card.Title><h1>{el.name}</h1></Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">{el.full_name}</Card.Subtitle>
+                                <Card.Subtitle className="mb-2 text-muted">Owner: {el.owner.login}</Card.Subtitle>
+                                <Card.Text> 
+                                    <h4 style={{margin: 0}}>private:</h4> 
+                                </Card.Text>
+                                <Card.Text> 
+                                    <p>{JSON.stringify(el.private, null,2 )}</p>
+                                </Card.Text>
+                                <Card.Text> 
+                                    <h4 style={{margin: 0}}>description: </h4>
+                                </Card.Text>
+                                <Card.Text> 
+                                    <p>{el.description}</p>
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                        </Card>
                 })
             }
         </Container>
